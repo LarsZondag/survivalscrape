1e3092a0dab19a46ab880f7b976d5d140284d5f7
Lars Zondag <larszondag@gmail.com>
2020-02-22 10:58:19 +0100

    rewrote parseGroups to use generic functions

---
+++ functions.js
@@ -0,0 +1,62 @@
+const R = require('ramda');
+const $ = require('cheerio');
+
+// plural of findIndex
+function findIndices(fn, list) {
+  var acc = [];
+  var idx = 0;
+  var len = list.length;
+  while (idx < len) {
+    if (fn(list[idx])) {
+      acc.push(idx);
+    }
+    idx += 1;
+  }
+  return acc;
+}
+
+// plural of splitWhen
+function splitIndices(indices, list) {
+  var acc = [];
+  var len = indices.length + 1;
+  var idx0 = 0;
+  var idx1 = indices[0];
+  var cnt = 0;
+  while (cnt < len) {
+    acc.push(R.slice(idx0, idx1, list))
+    idx0 = indices[cnt];
+    cnt++;
+    idx1 = indices[cnt];
+  }
+  return acc;
+}
+
+module.exports = {
+  tableToJson: function (table, options = {}) {
+    const default_options = {
+      skip_first_row: true,
+    }
+    options = R.merge(default_options, options);
+    let rows = table.find('tr');
+    if (options.skip_first_row) rows = R.drop(1, rows);
+
+    
+    return $(rows).map((i, row) => {
+      const cells = $(row).find('td');
+      values = {};
+      cells.each((index, el) => {
+        const value = $(el).text().trim();
+        const property = R.has('headers', options) ? options.headers[index] : index;
+        if (value !== '') values[property] = value;
+      });
+      return values;
+    }).get();
+  },
+
+  splitOn: function(fn, list) {
+    let indices = findIndices(fn, list);
+    indices = R.drop(1, indices);
+    return splitIndices(indices, list);
+  }
+  
+}
--- parseGroups.js
+++ parseGroups.js
@@ -1,6 +1,8 @@
 const siteUrl = "https://www.uvponline.nl/uvponlineU/index.php/uitslag/toonuitslagcat/794/1168/T";
 const axios = require("axios");
 const cheerio = require("cheerio");
+const functions = require("./functions");
+const R = require('ramda');
 
 const fetchData = async () => {
   const result = await axios.get(siteUrl);
@@ -10,46 +12,25 @@ const fetchData = async () => {
 fetchData().then(value => {
   $ = value;
   const uitslagen = $('table.tbl-border');
-  const runners = uitslagen.find('tr').slice(1).map((i, element) => ({
-    place: $(element).find('td:nth-of-type(1)').text().trim(),
-    groupname: $(element).find('td:nth-of-type(2)').text().trim(),
-    name: $(element).find('td:nth-of-type(3)').text().trim(),
-    lastname: $(element).find('td:nth-of-type(4)').text().trim(),
-    residence: $(element).find('td:nth-of-type(5)').text().trim(),
-    startnum: $(element).find('td:nth-of-type(6)').text().trim(),
-    time: $(element).find('td:nth-of-type(7)').text().trim(),
-    missedOb: $(element).find('td:nth-of-type(8)').text().trim(),
-    points: $(element).find('td:nth-of-type(9)').text().trim()
-  })).get();
+  const runners = functions.tableToJson(uitslagen, {
+    headers: [
+      'place',
+      'groupname',
+      'name',
+      'lastname',
+      'residence',
+      'startnum',
+      'time',
+      'missedOb',
+      'points'
+    ]
+  });
 
-  // Group data per group
-  var currentGroup = runners[0];
-  for (const runner of runners) {
-    
-    // Find first group member
-    if (runner.place != '') {
-        runner.name = [runner.name];
-        runner.lastname = [runner.lastname];
-        runner.residence = [runner.residence];
-        currentGroup = runner;
-    }
-
-    // Add other members to first member
-    else {
-        currentGroup.name.push(runner.name);
-        currentGroup.lastname.push(runner.lastname);
-        currentGroup.residence.push(runner.residence);
-    }
-  }
-
-  // Remove other group members extra data
-  for (i = 0; i < runners.length; i++) {
-      if (runners[i].place == '') {
-          runners.splice(i, 1);
-          i--;
-      }
-  }
-
-  console.log(runners);
-
+  let groups = functions.splitOn(R.has('groupname'), runners);
+  groups = R.map(participants => {
+    group = R.pick(['place', 'groupname', 'startnum', 'time', 'missedOb', 'points'], participants[0])
+    participants = R.map(R.pick(['name', 'lastname', 'residence']), participants);
+    return R.assoc('participants', participants, group);
+  }, groups)
+  console.log(groups);
 });
